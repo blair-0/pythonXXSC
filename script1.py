9fee2b57484e89001adeafbfc4886d53307cd366
yzh <you_zhihong@aliyun.com>
2018-11-11 21:44:08 +0800

    test

---
+++ script1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#!-*- coding:utf-8 -*-
+
+"""
+@Time: 18-10-23 上午12:53
+@Author: yzh
+@File: script1.py
+@Software: PyCharm
+"""
+
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)
